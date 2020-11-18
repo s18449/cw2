1b85eb83185a23a546c4cd9c75c1eda9a97576ae
StefanoCasciarri <stefan.casciarri@gmail.com>
2020-11-18 22:46:42 +0100

    Initial commit.

---
+++ Cw2/Cw2/Program.cs
@@ -0,0 +1,74 @@
+﻿using CsvHelper;
+using Cw2.models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Cw2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length > 3)
+            {
+                File.WriteAllText("łog.txt", "Incorrect number of arguments provided: " + args.Length + " arguments");
+                throw new ApplicationException("Incorrect number of arguments provided: " + args.Length + " arguments");
+            }
+
+            string CsvFileName = "dane.csv";
+            string ResultFileName = "żesult.xml";
+            string DataType = "xml";
+
+            if (args.Length == 3)
+            {
+                CsvFileName = args[0];
+                ResultFileName = args[1];
+                DataType = args[2];
+            } 
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(CsvFileName))
+                using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    Student student = new Student();
+                    csv.Configuration.HasHeaderRecord = false;
+                    List<Student> records = csv.GetRecords<Student>().ToList();
+
+                    for(int i = 0; i < records.Count; i++)
+                    {
+                        foreach(PropertyInfo propertyInfo in records.ElementAt(i).GetType().GetProperties())
+                        {
+                            if(propertyInfo.PropertyType == typeof(string))
+                            {
+                                string value = (string)propertyInfo.GetValue(records.ElementAt(i));
+                                if(string.IsNullOrEmpty(value))
+                                {
+                                    records.RemoveAt(i);
+                                }
+                            }
+                               
+                        }
+                    }
+
+                    FileStream writer = new FileStream(ResultFileName, FileMode.Create);
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+                    serializer.Serialize(writer, records);
+                    writer.Close();
+
+                    string jsonString = JsonSerializer.Serialize(records);
+                    File.WriteAllText("data.json", jsonString);
+                }
+            } catch (FileNotFoundException ex)
+            {
+                File.WriteAllText("łog.txt", ex.Message);
+            }
+        }
+    }
+}
---
+++ Cw2/Cw2/models/Student.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Cw2.models
+{
+    [Serializable]
+    public class Student
+    {
+        [Index(0)]
+        [JsonPropertyName("Imie")]
+        [XmlElement(ElementName = "Imie")]
+        public string FirstName { get; set; }
+
+        [Index(1)]
+        [XmlAttribute("Nazwisko")]
+        public string LastName { get; set; }
+
+        [Index(2)]
+        public string Studia { get; set; }
+
+        [Index(3)]
+        public string Mode { get; set; }
+
+        [Index(4)]
+        public string IndexNumber { get; set; }
+
+        [Index(5)]
+        public DateTime Birthdate { get; set; }
+
+        [Index(6)]
+        public string Mail { get; set; }
+
+        [Index(7)]
+        public string MotherFirstName { get; set; }
+
+        [Index(8)]
+        public string FatherFirstName { get; set; }
+
+    }
+}
